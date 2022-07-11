bb9c708a58998f74738636a97db6e83ce8990cd9
Tetsuo Koyama <tkoyama010@gmail.com>
2022-07-11 12:00:46 -0500

    Apply suggestions from code review

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -201,7 +201,7 @@ tutorial_dirs = [
     '../../tutorial/08_break/',
     '../../tutorial/09_sphinx/',
     '../../tutorial/10_qt/',
-    '../../tutorial/11_questions/',
+    '../../tutorial/12_questions/',
 ]
 
 sphinx_gallery_conf = {
