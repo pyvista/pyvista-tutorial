583b4e54e108b0212c1ccf1f0b65b96c58c1f073
Bane Sullivan <bane.sullivan@kitware.com>
2023-07-09 23:31:35 -0400

    Add Trame content

---
+++ tutorial/10_trame/d_trame_scalar_range.py
@@ -0,0 +1,55 @@
+"""
+Control Scalar Range
+~~~~~~~~~~~~~~~~~~~~
+
+Extending our simple example to control the color limits of the mapped scalars.
+"""
+import pyvista as pv
+from pyvista import examples
+from pyvista.trame.ui import plotter_ui
+from trame.app import get_server
+from trame.ui.vuetify import SinglePageLayout
+from trame.widgets import vuetify
+
+pv.OFF_SCREEN = True
+
+server = get_server()
+state, ctrl = server.state, server.controller
+
+mesh = pv.Wavelet()
+
+pl = pv.Plotter()
+actor = pl.add_mesh(mesh)
+
+
+@state.change("scalar_range")
+def set_scalar_range(scalar_range=mesh.get_data_range(), **kwargs):
+    actor.mapper.scalar_range = scalar_range
+    ctrl.view_update()
+
+
+with SinglePageLayout(server) as layout:
+    with layout.toolbar:
+        vuetify.VSpacer()
+        vuetify.VRangeSlider(
+            thumb_size=16,
+            thumb_label=True,
+            label="Range",
+            v_model=("scalar_range", [0, 300]),
+            min=('0',),
+            max=('500',),
+            dense=True,
+            hide_details=True,
+            style="max-width: 400px",
+        )
+
+    with layout.content:
+        with vuetify.VContainer(
+            fluid=True,
+            classes="pa-0 fill-height",
+        ):
+            # Use PyVista UI template for Plotters
+            view = plotter_ui(pl)
+            ctrl.view_update = view.update
+
+server.start()
---
+++ tutorial/10_trame/e_trame_algorithm.py
@@ -0,0 +1,61 @@
+"""
+Using VTK, PyVista, and Trame
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This example demonstrates how to use VTK, PyVista, and Trame together
+to show how the three libraries complement eachother.
+"""
+
+import pyvista as pv
+from pyvista.trame.ui import plotter_ui
+from trame.app import get_server
+from trame.ui.vuetify import SinglePageLayout
+from trame.widgets import vuetify
+from vtkmodules.vtkFiltersSources import vtkConeSource
+
+pv.OFF_SCREEN = True
+
+server = get_server()
+state, ctrl = server.state, server.controller
+
+source = vtkConeSource()
+
+pl = pv.Plotter()
+pl.add_mesh(source, color='seagreen')
+
+
+@state.change("resolution")
+def update_contour(resolution, **kwargs):
+    source.SetResolution(int(resolution))
+    ctrl.view_update()
+
+
+with SinglePageLayout(server) as layout:
+    with layout.toolbar:
+        vuetify.VSpacer()
+        vuetify.VSlider(
+            v_model=("resolution", 15),
+            min=5,
+            max=30,
+            hide_details=True,
+            dense=True,
+            style="max-width: 300px",
+            change=ctrl.view_update,
+        )
+        vuetify.VProgressLinear(
+            indeterminate=True,
+            absolute=True,
+            bottom=True,
+            active=("trame__busy",),
+        )
+
+    with layout.content:
+        with vuetify.VContainer(
+            fluid=True,
+            classes="pa-0 fill-height",
+        ):
+            # Use PyVista UI template for Plotters
+            view = plotter_ui(pl)
+            ctrl.view_update = view.update
+
+server.start()
