08bf12c21619e6ace6c95ac9606cc3a5ca2d4db8
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 17:33:42 +0900

    Update d_trame_scalar_range.py

--- tutorial/09_trame/d_trame_scalar_range.py
+++ tutorial/09_trame/d_trame_scalar_range.py
@@ -2,6 +2,11 @@
 Control Scalar Range
 ~~~~~~~~~~~~~~~~~~~~
 
+.. raw:: html
+    <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/09_trame/d_trame_scalar_range.ipynb">
+      <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+    </a>
+
 Extending our simple example to control the color limits of the mapped scalars.
 """
 import pyvista as pv
