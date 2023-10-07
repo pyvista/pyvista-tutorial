8f498529cbdd86fe5479059895aa0e0b72ff2bbd
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 15:52:46 +0900

    Update a_load_examples_exercise.py

--- tutorial/01_basic/exercises/a_load_examples_exercise.py
+++ tutorial/01_basic/exercises/a_load_examples_exercise.py
@@ -3,9 +3,10 @@
 
 Download and Plot Examples
 ~~~~~~~~~~~~~~~~~~~~~~~~~~
-<a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/01_basic/exercises/a_load_examples_exercise.ipynb">
-  <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
-</a>
+.. raw:: html
+    <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/01_basic/exercises/a_load_examples_exercise.ipynb">
+      <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+    </a>
 
 Download and plot example datasets.
 
