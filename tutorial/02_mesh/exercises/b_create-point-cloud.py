bcde4575705e1f909d281c5b4d67703f673842f6
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 15:54:31 +0900

    Update b_create-point-cloud.py

--- tutorial/02_mesh/exercises/b_create-point-cloud.py
+++ tutorial/02_mesh/exercises/b_create-point-cloud.py
@@ -3,9 +3,11 @@
 
 Create Point Cloud
 ~~~~~~~~~~~~~~~~~~
-<a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/02_mesh/exercises/b_create-point-cloud.ipynb">
-  <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
-</a>
+.. raw:: HTML
+
+    <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/02_mesh/exercises/b_create-point-cloud.ipynb">
+      <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+    </a>
 
 Create a :class:`pyvista.PolyData` object from a point cloud of vertices and
 scalar arrays for those points.
