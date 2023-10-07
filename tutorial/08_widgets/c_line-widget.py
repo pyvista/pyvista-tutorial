d1cbfe14a796c6a78e54947bbac3f6cdb22b76f2
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 17:28:13 +0900

    Update c_line-widget.py

--- tutorial/08_widgets/c_line-widget.py
+++ tutorial/08_widgets/c_line-widget.py
@@ -1,6 +1,11 @@
 """
 .. _line_widget_example:
 
+.. raw:: html
+    <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/08_widgets/c_line-widget.ipynb">
+      <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+    </a>
+
 Line Widget
 ~~~~~~~~~~~
 
