ae35b63f7987f1b955812fe9b0dc43b74be59ba2
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 17:02:57 +0900

    Update e_labels.py

--- tutorial/03_figures/bonus/e_labels.py
+++ tutorial/03_figures/bonus/e_labels.py
@@ -1,6 +1,10 @@
 """
 Label Points
 ~~~~~~~~~~~~
+.. raw:: html
+    <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/03_figures/bonus/e_labels.ipynb">
+      <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+    </a>
 
 Use string arrays in a point set to label points
 """
