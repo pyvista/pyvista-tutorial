a72d5b074d2a5f9cb81ccb0b5ef3a91c16d38d4b
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 16:37:37 +0900

    Update a_lesson_figures.py

--- tutorial/03_figures/a_lesson_figures.py
+++ tutorial/03_figures/a_lesson_figures.py
@@ -1,6 +1,11 @@
 """
 Lesson Overview
 ~~~~~~~~~~~~~~~
+.. raw:: html
+
+    <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/03_figures/a_lesson_figures.ipynb">
+      <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+    </a>
 
 """
 import pyvista as pv
