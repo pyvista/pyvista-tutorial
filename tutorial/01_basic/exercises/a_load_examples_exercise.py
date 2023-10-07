361efc87f712ac9ef782d699949ae51bd88ee820
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 22:59:50 +0900

    Update a_load_examples_exercise.py

--- tutorial/01_basic/exercises/a_load_examples_exercise.py
+++ tutorial/01_basic/exercises/a_load_examples_exercise.py
@@ -90,7 +90,7 @@ pl.show()
 # .. raw:: html
 #
 #     <center>
-#       <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/01_basic/a_load_examples_exercise.ipynb">
+#       <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/01_basic/exercises/a_load_examples_exercise.ipynb">
 #         <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/ width="150px">
 #       </a>
 #     </center>
