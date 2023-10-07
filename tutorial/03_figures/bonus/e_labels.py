4452018b8eb05062337382d23405d6e23be4da7f
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 17:09:43 +0900

    Update e_labels.py

--- tutorial/03_figures/bonus/e_labels.py
+++ tutorial/03_figures/bonus/e_labels.py
@@ -1,6 +1,7 @@
 """
 Label Points
 ~~~~~~~~~~~~
+
 .. raw:: html
     <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/03_figures/bonus/e_labels.ipynb">
       <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
