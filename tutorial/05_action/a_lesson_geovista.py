e608c2347482b0d42f65505507ce995b5fbf5b77
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 17:23:16 +0900

    Update a_lesson_geovista.py

--- tutorial/05_action/a_lesson_geovista.py
+++ tutorial/05_action/a_lesson_geovista.py
@@ -2,6 +2,11 @@
 Using GeoVista
 ~~~~~~~~~~~~~~
 
+.. raw:: html
+    <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/04_filters/a_lesson_geovista.ipynb">
+      <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+    </a>
+
 This is provided by `@bjlittle <https://github.com/bjlittle>`_ in
 `this discussion <https://github.com/bjlittle/geovista/discussions/343>`_
 and modified by `@tkoyama010 <https://github.com/tkoyama010>`_ .
