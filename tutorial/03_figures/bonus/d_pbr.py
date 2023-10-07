14ba4d6ca39f0d102cd89d08cdba32fce9b01028
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 17:02:15 +0900

    Update d_pbr.py

--- tutorial/03_figures/bonus/d_pbr.py
+++ tutorial/03_figures/bonus/d_pbr.py
@@ -3,6 +3,10 @@
 
 Physically Based Rendering
 ~~~~~~~~~~~~~~~~~~~~~~~~~~
+.. raw:: html
+    <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/03_figures/bonus/d_pbr.ipynb">
+      <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+    </a>
 
 VTK 9 introduced Physically Based Rendering (PBR) and we have exposed
 that functionality in PyVista. Read the `blog about PBR
