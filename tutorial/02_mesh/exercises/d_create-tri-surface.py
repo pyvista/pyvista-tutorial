7d19c3949abdd682ee71777ccfa8a572c858834c
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 23:27:54 +0900

    Update d_create-tri-surface.py

--- tutorial/02_mesh/exercises/d_create-tri-surface.py
+++ tutorial/02_mesh/exercises/d_create-tri-surface.py
@@ -89,6 +89,6 @@ surf.plot(cpos="xy", show_edges=True)
 #
 #     <center>
 #       <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/02_mesh/exercises/d_create-tri-surface.ipynb">
-#         <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+#         <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/ width="150px">
 #       </a>
 #     </center>
