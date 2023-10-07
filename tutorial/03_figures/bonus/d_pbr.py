f3291dd13cb004c513ee0077ac4424e761cf63b3
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 17:09:30 +0900

    Update d_pbr.py

--- tutorial/03_figures/bonus/d_pbr.py
+++ tutorial/03_figures/bonus/d_pbr.py
@@ -3,6 +3,7 @@
 
 Physically Based Rendering
 ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 .. raw:: html
     <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/03_figures/bonus/d_pbr.ipynb">
       <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
