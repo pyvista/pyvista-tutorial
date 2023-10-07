7288e9a047d3d24181c43c73752cf54d0383528e
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-07 17:22:33 +0900

    Update a_lesson_filters.py

--- tutorial/04_filters/a_lesson_filters.py
+++ tutorial/04_filters/a_lesson_filters.py
@@ -3,6 +3,7 @@
 
 Using Common Filters
 ~~~~~~~~~~~~~~~~~~~~
+
 .. raw:: html
     <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/04_filters/a_lesson_filters.ipynb">
       <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
