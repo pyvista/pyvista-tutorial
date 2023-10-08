a01486684f9742a2852ab721d8499712c1548427
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-08 10:37:15 +0900

    Update a_trame_simple.py

--- tutorial/09_trame/a_trame_simple.py
+++ tutorial/09_trame/a_trame_simple.py
@@ -2,11 +2,6 @@
 Simple Trame App
 ~~~~~~~~~~~~~~~~
 
-.. raw:: html
-    <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/09_trame/a_trame_simple.ipynb">
-      <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
-    </a>
-
 A simple example of how to create a Trame app with a PyVista Plotter.
 
 This example contains the boilerplate code to use anytime you are creating a
@@ -34,3 +29,12 @@ with SinglePageLayout(server) as layout:
         view = plotter_ui(pl)
 
 server.start()
+
+###############################################################################
+# .. raw:: html
+#
+#     <center>
+#       <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/09_trame/a_trame_simple.ipynb">
+#         <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/ width="150px">
+#       </a>
+#     </center>
