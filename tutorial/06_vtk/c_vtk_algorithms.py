00f372662d2f10899bca1ff9845d47c5be7742f7
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-08 09:57:20 +0900

    Update c_vtk_algorithms.py

--- tutorial/06_vtk/c_vtk_algorithms.py
+++ tutorial/06_vtk/c_vtk_algorithms.py
@@ -2,11 +2,6 @@
 Using VTK Algorithms
 ~~~~~~~~~~~~~~~~~~~~
 
-.. raw:: html
-    <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/06_vtk/c_vtk_algorithms.ipynb">
-      <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
-    </a>
-
 In this exercise, you will use a VTK Algorithm directly to filter a PyVista mesh.
 
 VTK algorithms (filters) follow a standard flow for most cases:
@@ -78,3 +73,12 @@ p.show()
 # to apply to your mesh. The instructors will be around to help you implement.
 #
 # See https://kitware.github.io/vtk-examples/site/Python/
+
+###############################################################################
+# .. raw:: html
+#
+#     <center>
+#       <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/06_vtk/c_vtk_algorithms.ipynb">
+#         <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/ width="150px">
+#       </a>
+#     </center>
