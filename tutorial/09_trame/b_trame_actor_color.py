28a6edcecd99651eff15988fde100388d879589a
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-08 10:38:13 +0900

    Update b_trame_actor_color.py

--- tutorial/09_trame/b_trame_actor_color.py
+++ tutorial/09_trame/b_trame_actor_color.py
@@ -2,11 +2,6 @@
 Control the Color of an Actor
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-.. raw:: html
-    <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/09_trame/b_trame_actor_color.ipynb">
-      <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
-    </a>
-
 Extending our simple example to have a dropdown menu to control the color of
 the actor.
 
@@ -59,3 +54,12 @@ with SinglePageLayout(server) as layout:
             ctrl.view_update = view.update
 
 server.start()
+
+###############################################################################
+# .. raw:: html
+#
+#     <center>
+#       <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/09_trame/b_trame_actor_color.ipynb">
+#         <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/ width="150px">
+#       </a>
+#     </center>
