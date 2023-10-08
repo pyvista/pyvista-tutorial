f07f10be38c0a12aed3b82a7fa38d41fc258cb34
Tetsuo Koyama <tkoyama010@gmail.com>
2023-10-08 10:42:50 +0900

    Update c_trame_scalars.py

--- tutorial/09_trame/c_trame_scalars.py
+++ tutorial/09_trame/c_trame_scalars.py
@@ -2,11 +2,6 @@
 Control Scalar Array
 ~~~~~~~~~~~~~~~~~~~~
 
-.. raw:: html
-    <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/09_trame/c_trame_scalars.ipynb">
-      <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
-    </a>
-
 Extending our simple example to have a dropdown menu to control which
 scalar array is used to color the mesh.
 """
@@ -74,3 +69,12 @@ with SinglePageLayout(server) as layout:
             ctrl.view_update = view.update
 
 server.start()
+
+###############################################################################
+# .. raw:: html
+#
+#     <center>
+#       <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/09_trame/c_trame_scalars.ipynb">
+#         <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/ width="150px">
+#       </a>
+#     </center>
