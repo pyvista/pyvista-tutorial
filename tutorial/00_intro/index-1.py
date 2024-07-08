db74994c532ccfcd853903d09654a70ea7ff2baa
banesullivan <banesullivan@users.noreply.github.com>
2024-07-08 22:18:31 +0000

    deploy: 71bdafe76e3f520a5e6434b43d75478013ffac57

--- tutorial/00_intro/index-1.py
+++ tutorial/00_intro/index-1.py
@@ -1,5 +1,3 @@
-import pyvista as pv
-import numpy as np
-points = np.random.random((1000, 3))
-pc = pv.PolyData(points)
-pc.plot(scalars=points[:, 2], point_size=5.0, cmap='jet')
+from pyvista import examples
+mesh = examples.download_bunny()
+mesh.plot(cpos='xy')
